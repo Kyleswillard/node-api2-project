7936d4b19ef8713121bf6f6e783faca648bb120d
Kyle Willard <kyleswillard@gmail.com>
2021-03-12 09:53:57 -0800

    kyle-willard

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+const express = require(express)
+const {find,
+  findById,
+  insert,
+  update,
+  remove} = require('./posts-model')
+const router = express.Router()
+
+router.get('/api/posts', async (req, res) => {
+const posts = await posts.find()
+})
